663bbd09dc5c2ebc2f19d753cdcb9a92a3321d11
Srinivas Kummari <srinivaskummari@Srinivass-MacBook-Pro.local>
2019-09-15 21:52:35 +0530

    Data Science with Python initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building a predictive model for titanic disaster recovery',
+    author='Srinivas Kummari',
+    license='MIT',
+)
